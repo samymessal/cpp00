e159e64180b4d28ed6f61711faf4f26c5379f538
Samy MESSAL <smessal@e1r9p16.clusters.42paris.fr>
2023-05-25 17:57:54 +0200

    finishing ex02 cpp00

---
+++ cpp00/ex02/Account.cpp
@@ -0,0 +1,115 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: smessal <smessal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/25 10:08:14 by smessal           #+#    #+#             */
+/*   Updated: 2023/05/25 17:55:59 by smessal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include "iostream"
+#include "ctime"
+
+Account::Account( int initial_deposit ) : _accountIndex(_nbAccounts), _amount(initial_deposit), _nbDeposits(0),
+											_nbWithdrawals(0)
+{
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";amount:" << _amount << "created" << std::endl;
+	_nbAccounts++;
+	_totalAmount += _amount;
+	return ;
+}
+
+Account::Account(void)
+{
+	std::cout << "Default constructor for Account called" << std::endl;
+	return ;
+}
+
+Account::~Account(void)
+{
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";amount:" << _amount << ";closed" << std::endl;
+	return ;
+}
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+void	Account::makeDeposit(int deposit)
+{
+	_amount += deposit;
+	_nbDeposits++;
+	_totalNbDeposits++;
+	_totalAmount += deposit;
+	return ;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	if (withdrawal <= _amount)
+	{
+		_amount -= withdrawal;
+		_nbWithdrawals++;
+		_totalNbWithdrawals++;
+		_totalAmount -= withdrawal;
+		return (1);
+	}
+	else
+		return (0);
+}
+
+int		Account::checkAmount(void) const
+{
+	return (this->_amount);
+}
+
+void	Account::_displayTimestamp( void )
+{
+    char buffer[20];
+	
+	std::time_t currentTime = std::time(NULL);
+    struct tm* timeInfo = std::localtime(&currentTime);
+    std::strftime(buffer, sizeof(buffer), "%Y%m%d_%H%M%S", timeInfo);
+	std::cout << "[" << buffer << "]";
+}
+
+void	Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	std::cout << " index:" << _accountIndex << ";amount:" << _amount << ";deposits:" << _nbDeposits << ";withdrawals:" << _nbWithdrawals << std::endl;
+	return ;
+}
+
+int		Account::getNbAccounts(void)
+{
+	return (_nbAccounts);
+}
+
+int		Account::getTotalAmount(void)
+{
+	return (_totalAmount);
+}
+
+int		Account::getNbDeposits(void)
+{
+	return (_totalNbDeposits);
+}
+
+int		Account::getNbWithdrawals(void)
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	_displayTimestamp();
+	std::cout << " accounts:" << _nbAccounts << ";total:" << _totalAmount << ";deposits:" << _totalNbDeposits << ";withdrawals:" << _totalNbWithdrawals << std::endl;
+	return ;
+}
